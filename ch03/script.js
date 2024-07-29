5bfed080d134e7b224f404da9869918dad2eb70c
aserafim <correia.alefe@gmail.com>
2024-07-29 13:50:16 -0300

    Capítulo 3 - Exercício 1 resolvido

---
+++ ch03/script.js
@@ -0,0 +1,28 @@
+/*
+var imprimeNome = function(nome) {
+    console.log(nome)
+}
+
+imprimeNome("Alefe")
+
+for (i = 0; i < 10; i++) {
+    var teste = "Nada"
+    console.log(teste)
+}
+console.log(teste)
+
+teste = "Novo"
+
+console.log(teste)
+*/
+
+//Exercício 1
+var getMin = function(x, y) {
+    if (x > y) {
+        return y
+    } else return x
+}
+
+console.log(getMin(3,2))
+
+//Exercício 2
