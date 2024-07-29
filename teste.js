2678a6ed4c3d510d32a9d2c5e7cf2048dd5e1c5c
aserafim <correia.alefe@gmail.com>
2024-07-29 11:26:45 -0300

    Resolvidos exercícios do capítulo 2

--- teste.js
+++ teste.js
@@ -1,3 +1,45 @@
-var teste = 3 * 4
-console.log(teste)
-alert("Tudo funcionando bem.")
+//Exercício 1
+/*
+char = "#"
+for (i = 0; i<8; i++) {
+    console.log(char);
+    char += "#"
+}
+*/
+
+//Exercício 2
+/*
+for (i = 1; i <= 100; i++) {
+    if (i % 5 == 0 && i % 3 == 0){
+        console.log("FizzBuzz")
+    } else if (i % 5 == 0 && i % 3 != 0) {
+        console.log("Buzz")
+    } else if (i % 5 != 0 && i % 3 == 0) {
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
+}
+*/
+//Exercício 3
+space = " "
+char2 = "#"
+line1 = ""
+line2 = ""
+charLimit = 8
+lineLimit = 4
+
+for (i = 0; i < charLimit; i++) {
+    if (i % 2 == 0){
+        line1 += char2;
+        line2 += space;
+    } else {
+        line1 += space;
+        line2 += char2;
+    }
+}
+
+for (i = 0; i < lineLimit; i++) {
+    console.log(line1)
+    console.log(line2)
+}
