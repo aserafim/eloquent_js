75c68775092b65b65b57fb1423af05550387faca
aserafim <correia.alefe@gmail.com>
2024-07-30 10:45:44 -0300

    Exercício 1 - Primeira parte

---
+++ ch04/script.js
@@ -0,0 +1,11 @@
+//Exercício 1
+
+var range = function(start, end){
+    lista = []
+    for (i = start; i <= end; i++){
+        lista.push(i);
+    }
+    return lista
+}
+
+//console.log(range(1,3));
